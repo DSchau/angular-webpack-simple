0fd3bac188789d6068e61edd9c27dc1d217082a8
Dustin Schau <dustinschau@gmail.com>
2016-07-26 14:04:39 -0500

    chore: remove use strict from webpack declaration

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,6 @@ const webpack = require('webpack');
 const merge = require('webpack-merge');
 
 module.exports = function(env) {
-  'use strict';
   const webpackEnv = env && env.prod && 'prod' || 'dev';
 
   return merge.smart(require('./webpack.config.common'), require(`./webpack.config.${webpackEnv}`));
